d97820aa6c2f7356d09296b543d68eb28d7b7031
Blinov <blinov@arcsinus.ru>
2020-12-02 10:27:02 +0300

    Исправил логические несостыковки, нейминг функций, force unwrapping

--- ToDoListFirebase/Controllers/LoginViewController.swift
+++ ToDoListFirebase/Controllers/LoginViewController.swift
@@ -11,24 +11,23 @@ import Firebase
 
 class LoginViewController: UIViewController {
     
-    var ref: DatabaseReference!
+	  private var ref: DatabaseReference!
 
-    @IBOutlet weak var warnLabel: UILabel!
-    @IBOutlet weak var emailTF: UITextField!
-    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet private weak var warnLabel: UILabel!
+    @IBOutlet private weak var emailTF: UITextField!
+    @IBOutlet private weak var passwordTF: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         ref = Database.database().reference(withPath: "users")
         
-        NotificationCenter.default.addObserver(self, selector: #selector(kbDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(kbDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidShow), name: UIResponder.keyboardDidShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardDidHide), name: UIResponder.keyboardDidHideNotification, object: nil)
         
         Auth.auth().addStateDidChangeListener { [weak self] (auth, user) in
-            if user != nil {
-                self?.performSegue(withIdentifier: "tasksSegue", sender: nil)
-            }
+					guard let _ = user else { return }
+					self?.performSegue(withIdentifier: TDLConstants.Segues.tasks, sender: nil)
         }
     }
     
@@ -40,9 +39,8 @@ class LoginViewController: UIViewController {
     
     func displayWarningLabel(withText text: String) {
         warnLabel.text = text
-        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {[weak self] in self?.warnLabel.alpha = 1}){
-            [weak self] complete in self?.warnLabel.alpha = 0
-        }
+        UIView.animate(withDuration: 3, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: { [weak self] in self?.warnLabel.alpha = 1})
+				{ [weak self] complete in self?.warnLabel.alpha = 0 }
         
     }
 
@@ -51,45 +49,49 @@ class LoginViewController: UIViewController {
             displayWarningLabel(withText: "info is incorrect")
             return
         }
+			
         Auth.auth().signIn(withEmail: email, password: password){ [weak self] (user, error) in
-            if error != nil {
+            if let _ = error {
                 self?.displayWarningLabel(withText: "Error ocured")
                 return
             }
-            if user != nil {
-                self?.performSegue(withIdentifier: "tasksSegue", sender: nil)
+            if let _ = user {
+                self?.performSegue(withIdentifier: TDLConstants.Segues.tasks, sender: nil)
                 return
             }
             self?.displayWarningLabel(withText: "No such user")
         }
-        
     }
+	
     @IBAction func registerTapped(_ sender: UIButton) {
-        guard let email = emailTF.text, let password = passwordTF.text, email != "", password != "" else {
-            displayWarningLabel(withText: "Info is incorrect")
-            return
-        }
-        Auth.auth().createUser(withEmail: email, password: password) { [weak self] (user, error) in
-            guard error == nil, user != nil else {
-                self?.displayWarningLabel(withText: "Registration was incorrect")
-                print(error!.localizedDescription)
-                return
-            }
-            
-            let userRef = self?.ref.child((user?.user.uid)!)
-            userRef?.setValue(["email": user?.user.email])
-        }
+			guard let email = emailTF.text, let password = passwordTF.text, email != "", password != "" else {
+				displayWarningLabel(withText: "Info is incorrect")
+				return
+			}
+			
+			Auth.auth().createUser(withEmail: email, password: password) { [weak self] (user, error) in
+				guard error == nil, user != nil else {
+					self?.displayWarningLabel(withText: "Registration was incorrect")
+					print(error!.localizedDescription)
+					return
+				}
+				
+				guard let user = user else { return }
+				let userRef = self?.ref.child(user.user.uid)
+				userRef?.setValue(["email": user.user.email])
+			}
     }
     
-    @objc func kbDidShow (notification: Notification) {
-        guard let userInfo = notification.userInfo else { return }
+    @objc func keyboardDidShow (notification: Notification) {
+			guard let userInfo = notification.userInfo, let scrollView = view as? UIScrollView else { return }
         let kbFrameSize = (userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
-        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height + kbFrameSize.height)
-        (self.view as! UIScrollView).scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
+			scrollView.contentSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height + kbFrameSize.height)
+			scrollView.scrollIndicatorInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbFrameSize.height, right: 0)
     }
     
-    @objc func kbDidHide () {
-        (self.view as! UIScrollView).contentSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height)
+    @objc func keyboardDidHide () {
+			guard let scrollView = view as? UIScrollView else { return }
+			scrollView.contentSize = CGSize(width: self.view.bounds.width, height: self.view.bounds.height)
     }
     
 }
---
+++ ToDoListFirebase/Helpers/TDLConstants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ToDoListFirebase
+//
+//  Created by Danil Blinov on 02.12.2020.
+//  Copyright © 2020 Николаев Никита. All rights reserved.
+//
+
+import Foundation
+
+struct TDLConstants {
+	struct Segues {
+		static let tasks = "tasksSegue"
+	}
+}
